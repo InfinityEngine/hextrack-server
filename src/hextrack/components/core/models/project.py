da869b0a5de4a15ef32e92985c7873d462532b5c
dimonzo <zibertscrem@gmail.com>
2018-01-28 04:19:29 +0200

    Added archived field to project

--- src/hextrack/components/core/models/project.py
+++ src/hextrack/components/core/models/project.py
@@ -4,4 +4,4 @@ from .common import AuditableModel
 
 class Project(AuditableModel):
     name = models.CharField(max_length=255, null=False, blank=False)
-
+    archived = models.BooleanField(null=False, blank=False, default=False)
